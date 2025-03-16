f6e108d9812648f0d9a97e639999434944a53ac7
Fahmi866 <muhammadzulfahmi180@student.uir.ac.id>
2025-03-16 15:15:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <input v-model="message" placeholder="Ketik pesan di sini" />
+
+    <button @click="showMessage">Tampilkan Pesan</button>
+
+    <p v-if="isMessageVisible">{{ message }}</p>
+
+    <p :class="{ 'highlight': isHighlighted }">Ini adalah teks dengan attribute binding.</p>
+
+    <button @click="toggleHighlight">Toggle Highlight</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const title = ref('Selamat Datang di Proyek Vue dengan Vite!');
+const message = ref('');
+const isMessageVisible = ref(false);
+const isHighlighted = ref(false);
+
+function showMessage() {
+  isMessageVisible.value = true;
+}
+
+function toggleHighlight() {
+  isHighlighted.value = !isHighlighted.value;
+}
+</script>
+
+<style>
+.highlight {
+  color: red;
+  font-weight: bold;
+}
+</style>
